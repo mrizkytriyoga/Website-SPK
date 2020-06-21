400523a70f3b5141bf64af23fb2a5fc895f1a369
mrizkytriyoga <48834983+mrizkytriyoga@users.noreply.github.com>
2020-06-21 15:33:59 +0700

    Website SPK

---
+++ webspk/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title >WEBSITE SPK</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" style="color: black"; href="">Sistem Pendukung Keputusan</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="index.php">HOME</a></li>
+                <li class="active"><a href="ahp.php">AHP</a></li>    
+                <li class="active"><a href="electree.php">ELECTREE</a></li>              
+                <li class="active"><a href="gdss.php">GDSS</a></li>
+                <li class="active"><a href="moora.php">MOORA</a></li>
+                <li class="active"><a href="saw.php"><b>SAW</b></a></li>
+                <li class="active"><a href="topsis.php"><b>TOPSIS</b></a></li>
+                <li class="active"><a href="wp.php"><b>WP</b></a></li>
+            
+            </ul>
+        </div>
+    </nav>  
+    
+    
+
+<html lang="en"
+<head>
+    <title>HOME</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery.js"></script>
+    <script src="js.bootstrap.js"></script>
+</head>
+<body>
+    <div class="container">
+    <div class="jumbotron">
+        <html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+
+
+</head>
+
+<body>
+
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+ 
+<div class="container">
+     <center> 
+        <h2>
+            <font style="color: black ;font-size:40px; font-family: Copperplate Gothic Light"><b>M RIZKY TRIYOGA</b></font></h2></font><br>
+            <span style="color: black ;font-size:40px; font-family: Copperplate Gothic Light"><b>1741720176</b></span></h2><br>
+            <span style="color: black ;font-size:40px; font-family: Copperplate Gothic Light"><b>TI-3C</b></span></h2>
+  <h3></h3>
+
+</body>
+</html>
+
+
+</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</center>
+</div>
+</body>
+
+</body>
+</html>
